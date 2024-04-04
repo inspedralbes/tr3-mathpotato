0d7ba516b00e025e6baf3d2c65e4b66d8220c7fc
rublormar <a21rublormar@inspedralbes.cat>
2024-04-04 12:22:31 +0200

    dawasda

--- web/mathpotato/nuxt.config.ts
+++ web/mathpotato/nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  server: {
+    host: '0'
+  },
   css: ['./assets/css/normalize.css', 'primevue/resources/themes/lara-light-teal/theme.css', 'primeicons/primeicons.css'],
   devtools: { enabled: true },
-
   modules: [
     '@pinia/nuxt',
     'nuxt-primevue',
